5baf28cc9b8ffd28fab13eedb684a931d37ac3bb
Peter Pistorius <peterp@196-210-142-195-tbnb-esr-3.dynamic.isadsl.co.za>
2009-07-18 00:59:37 +0200

    Rewriting javascript routines. Seperating logic from presentation.

--- wwwroot/media/js/jquery-airship.js
+++ wwwroot/media/js/jquery-airship.js
@@ -1,115 +1,197 @@
-(function($) {
+
+// FileManager, performs all the basic file management things that a person should expect.
+
+;(function($) {
     
+    var relativePath = '';
+    
+    $.fn.fileManager = function(atPath) {
+        relativePath = atPath;
+        this.ls = ls;
+        this.rm = rm;
+        this.mv = mv;
+        this.mkdir = mkdir;
+        return this;
+    };
+    
+    function ls(callback)
+    {
+        $.getJSON('/' + relativePath + '?format=json', function(r) {
+            // run the callback function...
+            callback(r);
+        });
+    };
+    
+    function mkdir(name)
+    {
+        var params = { 'directoryName': name, 'relativePath': relativePath };
+        $.post('/__/directory/create/', params, function(r) {
+            var r = r.split(';');
+            if (r.length == 0) {
+                // error;
+                return;
+            }
 
-    var relativePath = 'Storage';
+            if (parseInt(r, 10) > 0) {
+                // success
+            } else {
+                // error!
+                alert(r[1]);
+            }
+        });
+    };
+    
+    function rm()
+    {
+    };
+    
+    
+    function mv(oldName, newName)
+    {
+    };
+    // list directory items.
+    // delete
+    // createDirectory
+    // rename
+    // move (should be the same as rename)
+    
+}(jQuery));
 
-    $.fn.airship = function() {
-        
+
+
+// User interface stuff.......
+
+;(function($) {
+    
+    var currentRelativePath;
+    var fileManager;
+    
+    $.fn.airshipUI = function() {
         
         // upload
         $('#action-file-upload').fancybox({
             'hideOnContentClick': false,
-			'padding': 18,
-			'callbackOnStart': function() {
-			    var href = 'upload.html?relativePath=' + relativePath;
-			    $('#action-file-upload').attr('href', href);
-			}
+            'padding': 18,
+            'callbackOnStart': function() {
+                var href = 'upload.html?relativePath=' + relativePath;
+                $('#action-file-upload').attr('href', href);
+            }
         });
         
         // create folder
         $('#action-directory-create').click(function() {
-            // Insert A new Row called untitled folder.
-            
-            var d = new Date()
-            var ul = $('<ul></ul>')
-                .append('<li class="type ' + " " + '"></li>')
-                .append('<li class="name"><input type="text" name="directoryName" id="directoryName" value="untitled folder"></li>')
-                .append('<li class="date">Today, ' + d.getHours() + ':' + d.getMinutes() + '</li>')
-                .append('<li class="size">' + '--' + '</li>')
-            ul.prependTo('#item-list');
-            
-            renameItem(ul);
+            // var d = new Date()
+            // var ul = createItemRow('directory', 'untitled folder', 'Today, ' + d.getHours + ':' + d.getMinutes(), '--');
+            // $('#item-list').append(ul);
+            // renameItem(ul);
         });
-        directoryContentsAtPath(relativePath);
-    };
-    
-    var directoryContentsAtPath = function(atPath)
-    {
-        relativePath = atPath;
         
-        $.getJSON('/' + atPath + '?format=json', function(r) {
-            var list = $('#item-list');
-            list.html('');
-            if (r.length == 0) {
-                // this folder is empty.
-            } else {
-                $(r).each(function() {
-                    var ul = $('<ul></ul>')
-                        .append('<li class="type ' + this.type + '"></li>')
-                        .append('<li class="name">' + this.name + '</li>')
-                        .append('<li class="date">' + this.date + '</li>')
-                        .append('<li class="size">' + '--' + '</li>')
-                    ul.appendTo(list)
-                    
-                });
-                list.find('.name').click(openItem);
-            }
-        });
-    };
-    
-    var openItem = function()
-    {
-        // figure out what type this is?
-        var r = $(this).parent();
-        var type = r.find('.type')[0].className.split(' ')[1];
-        var name = r.find('.name').text()
         
-        if (type == 'directory') {
-            // change the directory path...
-            // reload the thingum....
-            directoryContentsAtPath(relativePath + '/' + name);
-        }
+        // load up the "Storage" directory.
+        loadDirectoryItems('Storage');
     };
     
-    var renameItem = function(row)
+    var loadDirectoryItems = function(atPath)
     {
-        // find the input element in this row
-        var input = row.find('input[type=text]');
-        if (input.length) {
-            // creating a new file.
-            var action = 'create';
-        } else {
-            // editing an existing file.
-            var action = 'rename';
-        }
-        
-        input.blur(function() {
-            // save on blur.
-            
-            $.post('/__/directory/' + action + '/', {'directoryName': input.val(), 'relativePath': relativePath}, function(r) {
-                
-                var r = r.split(';');
-                if (r.length == 0) {
-                    // error;
-                    return;
-                }
-                
-                if (parseInt(r, 10) > 0) {
-                    // success, revert back to textmode.
-                    input.parent().html(input.val());
-                } else {
-                    // error!
-                    alert(r[1]);
-                }
-            });
-        }).keydown(function(e) {
-            if (e.keyCode == 27) {
-                $(this).parent().parent().remove();
-            } else if (e.keyCode == 13) {
-                $(this).blur();
-            }
-        }).select().focus();
-    };
-    
+        currentRelativePath = atPath;
+        fileManager = $().fileManager(atPath);
 
-})(jQuery);
+        var list = $('#item-list');
+        list.html('');
+        
+        fileManager.ls(function(r) {
+            $(r).each(function(i) {
+                list.append(createItemRow(this.type, this.name, this.date, this.size));
+            });
+        });
+    };
+    
+    
+    // given the parameters it returns the appropriate row with 
+    // the proper events attached.
+    var createItemRow = function(type, name, date, size, mode)
+    {
+        var ul = $('<ul/>');
+        $('<li class="type"></li>').appendTo(ul).addClass(type);
+        $('<li class="name"></li>').appendTo(ul).html(name);
+        $('<li class="date"></li>').appendTo(ul).html(date);
+        $('<li class="size"></li>').appendTo(ul).html(size);
+        
+        return ul;
+    };
+    //   
+    //   var createItemRow = function(type, name, date, size)
+    //   {
+    //       var ul = $('<ul></ul>')
+    //           .append('<li class="type ' + type + '"></li>')
+    //           .append('<li class="name">' + name + '</li>')
+    //           .append('<li class="date">' + date + '</li>')
+    //           .append('<li class="size">' + size + '</li>');
+    //           
+    //       ul.find('.name').click(openItem);
+    //   };
+    //   
+    //   
+    //   var openItem = function()
+    //   {
+    //       // figure out what type this is?
+    //       var r = $(this).parent();
+    //       var type = r.find('.type')[0].className.split(' ')[1];
+    //       var name = r.find('.name').text();
+    //       
+    //       if (type == 'directory') {
+    //           // change the directory path...
+    //           // reload the thingum....
+    //           directoryContentsAtPath(relativePath + '/' + name);
+    //       }
+    //       
+    //       return false;
+    //   };
+    //   
+    //   var renameItem = function(row)
+    //   {
+    //       // conver the items name into a input.
+    //       row.find('.name').unbind('click').
+    //       
+    //       
+    //       // find the input element in this row
+    //       var input = row.find('input[type=text]');
+    //       if (input.length) {
+    //           // creating a new file.
+    //           var action = 'create';
+    //       } else {
+    //           // editing an existing file.
+    //           var action = 'rename';
+    //       }
+    //       
+    //       input.blur(function() {
+    //           // save on blur.
+    //           
+    //           $.post('/__/directory/' + action + '/', {'directoryName': input.val(), 'relativePath': relativePath}, function(r) {
+    //               
+    //               var r = r.split(';');
+    //               if (r.length == 0) {
+    //                   // error;
+    //                   return;
+    //               }
+    //               
+    //               if (parseInt(r, 10) > 0) {
+    //                   // success, revert back to textmode.
+    //                   console.log(input.parent());
+    //                   input.parent().click(openItem);
+    //                   input.parent().html(input.val());
+    // 
+    //               } else {
+    //                   // error!
+    //                   alert(r[1]);
+    //               }
+    //           });
+    //       }).keydown(function(e) {
+    //           if (e.keyCode == 27) {
+    //               $(this).parent().parent().remove();
+    //           } else if (e.keyCode == 13) {
+    //               $(this).blur();
+    //           }
+    //       });
+    //   };
+}(jQuery));
