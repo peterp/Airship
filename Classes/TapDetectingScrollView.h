ac763465d8d41ca6da541df71bc601d9cc34b411
Peter Pistorius <peterp@appfactory.co.za>
2009-11-03 23:02:48 +0200

    Taps are now detected on images / scrolls / zoom and toolbars are hidden accordingly.

---
+++ Classes/TapDetectingScrollView.h
@@ -0,0 +1,15 @@
+//
+//  TapDetectingScrollView.h
+//  Humboldt
+//
+//  Created by Peter Pistorius on 2009/08/23.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TapDetectingScrollView : UIScrollView {
+}
+
+@end
---
+++ Classes/TapDetectingScrollView.m
@@ -0,0 +1,32 @@
+//
+//  TapDetectingScrollView.m
+//  Humboldt
+//
+//  Created by Peter Pistorius on 2009/08/23.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "TapDetectingScrollView.h"
+
+
+@implementation TapDetectingScrollView
+
+
+- (id)initWithFrame:(CGRect)frame;
+{
+	return [super initWithFrame:frame];
+}
+
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event;
+{
+	NSLog(@"margle");
+	
+	if (!self.dragging) {
+		[self.nextResponder touchesEnded:touches withEvent:event]; 
+	} else {
+		[super touchesEnded:touches withEvent:event];
+	}
+}
+
+@end
